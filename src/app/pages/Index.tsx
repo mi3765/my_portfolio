5a3fac86c1b6a96b4c32f67f65ee6c12d31cd05e
mi3765 <chips1225@outlook.jp>
2023-08-29 01:10:23 +0900

    create new each failes

---
+++ src/app/pages/Index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+    return (
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Work</li>
+                <li>Blog</li>
+            </ul>
+        </div>
+    )
+}
