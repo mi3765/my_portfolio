c1cf76912d977f8d5d9e0ddca785a912ea22f864
mi3765 <chips1225@outlook.jp>
2023-09-13 15:49:54 +0900

    modify post page

--- src/app/pages/postarticle/page.tsx
+++ src/app/pages/postarticle/page.tsx
@@ -1,22 +1,56 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import { Header } from "@/app/components/Header";
+import axios from "axios";
 
 const PostArticle: React.FC = () => {
+	const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+	const [imagePreviews, setImagePreviews] = useState<string[]>([]);
+	const [title, setTitle] = useState("");
+	const [message, setMessage] = useState("");
+
 	const handleUploadPhoto = () => {
 		const fileInput = document.createElement("input");
 		fileInput.type = "file";
 		fileInput.accept = "image/*";
+		fileInput.multiple = true; // 複数のファイルを選択できるようにする
 		fileInput.click();
 
-		fileInput.addEventListener("change", (event) => {
-			const selectedFile = event.target.files?.[0];
-			if (selectedFile) {
-				// 選択されたファイルを処理する（アップロード、プレビューなど）
-				console.log("選択されたファイル:", selectedFile);
+		fileInput.addEventListener("change", (e) => {
+			const files = e.target.files;
+
+			if (files) {
+				// 最大4枚まで選択できるように制限する
+				const selectedFilesArray = Array.from(files).slice(0, 4);
+				setSelectedFiles(selectedFilesArray);
+
+				// 選択された画像をプレビュー表示
+				const previews = selectedFilesArray.map((file) =>
+					URL.createObjectURL(file)
+				);
+
+				// 既存のプレビュー画像に新しいプレビュー画像を追加
+				setImagePreviews((prevPreviews) => [...prevPreviews, ...previews]);
 			}
 		});
+	};
+
+	const postArticle = async () => {
+		try {
+			const articleData = new FormData();
+			articleData.append("title", title);
+			articleData.append("message", message);
+
+			selectedFiles.forEach((file, index) => {
+				articleData.append(`file${index}`, file);
+			});
+
+			const response = await axios.post("api/postarticle", articleData);
+			console.log("サーバーレスポンス", response.data);
+		} catch (error) {
+			console.log("エラー", error);
+		}
 	};
 
 	return (
@@ -25,19 +59,21 @@ const PostArticle: React.FC = () => {
 			<Header />
 			<div className="flex flex-col items-center">
 				<div className="flex items-center gap-5 m-5">
-					<label htmlFor="name" className="block text-gray-700 font-medium">
+					<label htmlFor="title" className="block text-gray-700 font-medium">
 						記事名
 					</label>
 					<input
 						type="text"
-						id="name"
-						name="name"
+						id="title"
+						name="title"
 						className="mt-1 p-1 max-w-lg border rounded-md focus:ring focus:ring-blue-200"
 						required
+						onChange={(e) => setTitle(e.target.value)}
+						value={title}
 					/>
 				</div>
 				<div className="flex items-center gap-5 m-5">
-					<h1>表紙画像</h1>
+					<h1>画像</h1>
 					<div>
 						<AddPhotoAlternateIcon
 							style={{ fontSize: "3rem" }}
@@ -46,11 +82,17 @@ const PostArticle: React.FC = () => {
 						/>
 					</div>
 				</div>
-				<img
-					src="/wave-photo.avif"
-					alt="wave-photo"
-					className="max-w-sm max-h-full"
-				/>
+				<div className="flex">
+					{imagePreviews.map((preview, index) => (
+						<div key={index} className="mr-2">
+							<img
+								src={preview}
+								alt={`Selected ${index + 1}`}
+								style={{ maxWidth: "100px", maxHeight: "100px" }}
+							/>
+						</div>
+					))}
+				</div>
 				<div className="flex items-center gap-5 m-5">
 					<label htmlFor="message" className="block text-gray-700 font-medium">
 						概要
@@ -61,6 +103,8 @@ const PostArticle: React.FC = () => {
 						className="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200"
 						rows={4}
 						required
+						onChange={(e) => setMessage(e.target.value)}
+						value={title}
 					></textarea>
 				</div>
 				<div className="mt-4">
